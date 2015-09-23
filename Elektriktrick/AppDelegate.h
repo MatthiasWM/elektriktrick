d04a8077e8d5cce73c2498007cf57f570198f1b1
Matthias Melcher <git@matthiasm.com>
2015-09-23 22:15:58 +0200

    Added minimal application containing the Quicklook plugin.

---
+++ Elektriktrick/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Elektriktrick
+//
+//  Created by Matthias Melcher on 9/23/15.
+//  Copyright © 2015 M.Melcher GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Elektriktrick/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Elektriktrick
+//
+//  Created by Matthias Melcher on 9/23/15.
+//  Copyright © 2015 M.Melcher GmbH. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
